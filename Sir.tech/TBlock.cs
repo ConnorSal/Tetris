23414dec6eb049bd24796a684b6fabad09796644
Connor <connorsal2005@gmail.com>
2025-10-22 18:02:38 -0400

    Fixed Id Property

--- Sir.tech/TBlock.cs
+++ Sir.tech/TBlock.cs
@@ -16,7 +16,7 @@ namespace Sir.tech
             new Position[] { new(0, 1), new(1, 0), new(1, 1), new(2, 1) }
         };
 
-        public override int Id => 1; 
+        public override int Id => 6; 
         protected override Position StartOffset => new Position(0, 3);
         protected override Position[][] Tiles => tiles;
     }
