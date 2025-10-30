326f5daffe7110d59d95c7d0aeea28b7fd9e6b45
Connor <connorsal2005@gmail.com>
2025-10-29 21:15:13 -0400

    Add Skin class with properties for skin details

---
+++ Sir.tech/Skin.cs
@@ -0,0 +1,11 @@
+namespace Sir.tech
+{
+    public class Skin
+    {
+        public string? Id { get; set; } 
+        public string? DisplayName { get; set; }
+        public string? ImagePath { get; set; }
+        public string? TilePath { get; set; }
+        public int Price { get; set; }
+    }
+}
