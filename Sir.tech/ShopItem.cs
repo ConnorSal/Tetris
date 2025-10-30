cb4fcfed8ad33c6bd0bc1487a9610ca792feb205
Connor <connorsal2005@gmail.com>
2025-10-29 21:15:56 -0400

    Add ShopItem class with BlockType and Skins properties

--- Sir.tech/ShopItem.cs
+++ Sir.tech/ShopItem.cs
@@ -1,1 +1,10 @@
+using System.Collections.Generic;
 
+namespace Sir.tech
+{
+    public class ShopItem
+    {
+        public string? BlockType { get; set; }
+        public List<Skin> Skins { get; set; } = new();
+    }
+}
